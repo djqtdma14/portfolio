4e7540e6f9d74cefd73c7d9576c4f54699a1f807
djqtdma14 <jhram7475@gmail.com>
2025-10-17 17:06:57 +0900

    portfolio

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,19 +9,21 @@ function Profile(){
   return(
     <section className='Profile'>
       <h2>PROFILE</h2>
-      <div className='about-me'>
-        <h3>about me</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, nulla incidunt eligendi quae maiores cum ea impedit reprehenderit esse unde doloremque? Laboriosam obcaecati eligendi est dolore accusantium voluptatibus perspiciatis. Est!
-        </p>
-      </div>
-      <div className='about-skill'>
-        <h3>skills</h3>
-        {
-          prontLogos.map((logo, idx) => {
-            return <img src={logo} alt={logo.keys} key={idx} />
-          })
-        }
+      <div className='about'>
+        <section className='about-me'>
+          <h3>about me</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, nulla incidunt eligendi quae maiores cum ea impedit reprehenderit esse unde doloremque? Laboriosam obcaecati eligendi est dolore accusantium voluptatibus perspiciatis. Est!
+          </p>
+        </section>
+        <section className='about-skill'>
+          <h3>skills</h3>
+          {
+            prontLogos.map((logo, idx) => {
+              return <img src={logo} alt={logo.keys} key={idx} />
+            })
+          }
+        </section>
       </div>
     </section>
   );
