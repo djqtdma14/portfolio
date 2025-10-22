1c06112ab46ae6089d8bbfdd389b6f34911f789b
djqtdma14 <jhram7475@gmail.com>
2025-10-22 17:10:53 +0900

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import './scss/App.scss';
 import FristPage from './pages/FirstPage.jsx';
 import Project from './pages/Project.jsx';
 import Profile from './pages/Profile.jsx';
+import Contact from './pages/Contact.jsx';
 // image by https://icons8.com/, https://techicons.dev/
 
 function App(){
-  // https://inthedev.tistory.com/27 - 스크롤 기능
+  // https://inthedev.tistory.com/27 - 스크롤 기능 사용
   const scrollRef = useRef([]);
   const handleScrollView = (e) => {
     const name = e.target.innerText;
     const pages = {
-      main: 0,
-      profile: 1,
-      project: 2,
+      main : 0,
+      profile : 1,
+      project : 2,
+      contact : 3
     };
     scrollRef.current[pages[name]].scrollIntoView({behavior:'smooth'});
   };
@@ -21,10 +23,11 @@ function App(){
   return(
     <section className='App'>
       <div className='Navbar' onClick={handleScrollView}>
-        <span className='main'>main</span>
+        <span className='main menu'>main</span>
         <nav>
-          <span className='profile'>profile</span>
-          <span className='project'>project</span>
+          <span className='profile menu'>profile</span>
+          <span className='project menu'>project</span>
+          <span className='contact menu'>contact</span>
         </nav>
       </div>
       <div ref={(el) => (scrollRef.current[0] = el)}>
@@ -35,6 +38,9 @@ function App(){
       </div>
       <div ref={(el) => (scrollRef.current[2] = el)}>
         <Project />
+      </div>
+      <div ref={(el) => (scrollRef.current[3] = el)}>
+        <Contact />
       </div>
     </section>
   );
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../scss/Contact.scss';
+import github from '../assets/images/toolLogos/GIthub.svg';
+
+function Contact(){
+  return(
+    <section className='Contact'>
+      <h2>CONTACT</h2>
+      <div className='git'>
+        <a href='https://github.com/djqtdma14' target='_blank'>
+          <img src={github} alt='github.svg' />
+          <span>깃허브 주소</span>
+        </a>
+      </div>
+      <a href='https://github.com/djqtdma14' target='_blank' className='github-link'>
+        <img src={github} alt='github.svg' />
+        <span>GitHub</span>
+      </a>
+    </section>
+  );
+};
+
+export default Contact;
