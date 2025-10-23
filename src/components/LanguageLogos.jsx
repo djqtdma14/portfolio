0e85fac909e2ed09768768322f31dbee9aba1cc7
djqtdma14 <jhram7475@gmail.com>
2025-10-23 10:20:51 +0900

    portfolio

--- src/components/LanguageLogos.jsx
+++ src/components/LanguageLogos.jsx
@@ -2,7 +2,7 @@ const LanguageLogos = {
   HTML5 : new URL('../assets/images/languageLogos/HTML5.svg', import.meta.url).href,
   CSS3 : new URL('../assets/images/languageLogos/CSS3.svg', import.meta.url).href,
   Sass : new URL('../assets/images/languageLogos/Sass.svg', import.meta.url).href,
-  javaScript : new URL('../assets/images/languageLogos/JavaScript.svg', import.meta.url).href,
+  JavaScript : new URL('../assets/images/languageLogos/JavaScript.svg', import.meta.url).href,
   React : new URL('../assets/images/languageLogos/React.svg', import.meta.url).href,
   Vue : new URL('../assets/images/languageLogos/Vue.js.svg', import.meta.url).href,
   Vite : new URL('../assets/images/languageLogos/Vite.js.svg', import.meta.url).href,
